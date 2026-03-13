88bb4e704ec5932f265cef7a6853455342d85668
Jennifer <1001.26480892.ucla@gmail.com>
2026-03-13 08:27:54 -0400

    estructura base del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="flex justify-center items-center gap-4 mb-8">
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo w-32 h-32" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react w-32 h-32" alt="React logo" />
         </a>
       </div>
-      <h1>PROMO 69</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1 className="text-center text-red-800 text-6xl mb-4">PROMO 69</h1>
+      <p className="read-the-docs text-center text-black-700 text-xl space-x-3">
+        Pagina web principal  
       </p>
     </>
   )
