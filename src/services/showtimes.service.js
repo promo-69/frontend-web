e344d4b78b7d575645bb6213e8e77a5f07fb2b43
Jennifer <1001.26480892.ucla@gmail.com>
2026-06-09 15:53:22 -0400

    filtrado de pelicula por sucursal

--- src/services/showtimes.service.js
+++ src/services/showtimes.service.js
@@ -1,4 +1,64 @@
-import api from '../api/axios'
+import api, { apiPublic } from '../api/axios'
+
+// ⭐ 1) Funciones de una película en una sucursal
+export const getShowtimesByMovieAndCinema = async (cinemaId, movieId) => {
+  const res = await apiPublic.get(
+    `/cinemas/${cinemaId}/showtimes/movies/${movieId}`,
+  )
+  return res.data.data
+}
+
+// ⭐ 2) Detalle de función por sucursal
+export const getShowtimeById = async (cinemaId, showtimeId) => {
+  const res = await apiPublic.get(
+    `/cinemas/${cinemaId}/showtimes/${showtimeId}`,
+  )
+  return res.data.data
+}
+
+// ⭐ 3) Mapa de asientos por sucursal
+export const getSeatMap = async (cinemaId, showtimeId) => {
+  const res = await api.get(
+    `/cinemas/${cinemaId}/showtimes/${showtimeId}/seat-map`,
+  )
+  return res.data.data
+}
+
+// ⭐ 4) Funciones disponibles por fecha (opcional)
+export const getShowtimesByDate = async (cinemaId, date) => {
+  const res = await apiPublic.get(`/cinemas/${cinemaId}/showtimes?date=${date}`)
+  return res.data.data
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+  /* 
+
+  import api from '../api/axios'
 import { apiPublic } from '../api/axios'
 
 export const getShowtimesByCinema = async (cinemaId) => {
@@ -18,5 +78,8 @@ export const getShowtimeById = async (showtimeId) => {
 
 export const getSeatMap = async (showtimeId) => {
   const response = await api.get(`/showtimes/${showtimeId}/seat-map`)
-  return response.data.data 
+  return response.data.data
 }
+*/
+}
+
