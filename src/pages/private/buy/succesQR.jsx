940aeaa3f12220d6fecc908751fa3980ac71b753
Jennifer <1001.26480892.ucla@gmail.com>
2026-06-10 04:07:35 -0400

    estructura inicial orders: chekout del carrito y proceso de pago, solo interfaz

--- src/pages/private/buy/succesQR.jsx
+++
@@ -1,7 +0,0 @@
-export default function Success() {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-white font-montserrat">
-      <h1 className="text-3xl font-bold">¡Compra realizada con éxito!</h1>
-    </div>
-  )
-}
